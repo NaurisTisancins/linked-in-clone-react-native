bf68c56e513cbde8132b8f0b47a7845e0fc1798a
Nauris Tisancins <nauristisanchins@gmail.com>
2023-09-25 16:18:51 +0100

    setup tabs for bottom tab nav

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={24} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
@@ -20,35 +20,60 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#191919',
       }}
     >
       <Tabs.Screen
         name='index'
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
-          headerRight: () => (
-            <Link href='/modal' asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name='info-circle'
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          title: 'Home feed',
+          tabBarIcon: ({ color }) => <TabBarIcon name='home' color={color} />,
+          // headerRight: () => (
+          //   <Link href='/modal' asChild>
+          //     <Pressable>
+          //       {({ pressed }) => (
+          //         <FontAwesome
+          //           name='info-circle'
+          //           size={25}
+          //           color={Colors[colorScheme ?? 'light'].text}
+          //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+          //         />
+          //       )}
+          //     </Pressable>
+          //   </Link>
+          // ),
+        }}
+      />
+      <Tabs.Screen
+        name='network'
+        options={{
+          title: 'My Network',
+          tabBarIcon: ({ color }) => <TabBarIcon name='group' color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='new-post'
+        options={{
+          title: 'New Post',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name='plus-square' color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name='two'
+        name='notifications'
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+          title: 'Notifications',
+          tabBarIcon: ({ color }) => <TabBarIcon name='bell' color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='jobs'
+        options={{
+          title: 'Jobs',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name='briefcase' color={color} />
+          ),
         }}
       />
     </Tabs>
