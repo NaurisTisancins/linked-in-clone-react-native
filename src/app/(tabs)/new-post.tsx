16087ab2a41271997d8d8b955d69cd41bf94284d
Nauris Tisancins <nauristisanchins@gmail.com>
2023-09-27 15:13:01 +0100

    expo-image-picker

--- src/app/(tabs)/new-post.tsx
+++ src/app/(tabs)/new-post.tsx
@@ -1,20 +1,27 @@
 import { useNavigation, useRouter } from 'expo-router';
 import { useLayoutEffect, useState } from 'react';
-import { Pressable, StyleSheet, TextInput } from 'react-native';
-
-import { Text, View } from '../../components/Themed';
+import {
+  Pressable,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function NewPostScreen() {
   const [content, setContent] = useState('');
+  const [image, setImage] = useState<string | null>(null);
 
   const navigation = useNavigation();
   const router = useRouter();
 
   const onPost = () => {
-    console.warn('Pressed', content);
-
     router.push('/(tabs)/');
     setContent('');
+    setImage(null);
   };
 
   useLayoutEffect(() => {
@@ -27,6 +34,20 @@ export default function NewPostScreen() {
     });
   }, [onPost]);
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      // aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -36,6 +57,20 @@ export default function NewPostScreen() {
         style={styles.input}
         multiline
       />
+
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+
+      <View style={styles.footer}>
+        <Pressable onPress={pickImage} style={styles.iconButton}>
+          <FontAwesome name='image' size={24} color='black' />
+        </Pressable>
+        <View style={styles.iconButton}>
+          <FontAwesome name='camera' size={24} color='black' />
+        </View>
+        <View style={styles.iconButton}>
+          <FontAwesome name='glass' size={24} color='black' />
+        </View>
+      </View>
     </View>
   );
 }
@@ -59,6 +94,23 @@ const styles = StyleSheet.create({
   },
   postButtonText: {
     color: 'white',
-    foontWeight: 'bold',
+    fontWeight: 'bold',
+  },
+
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    marginTop: 'auto',
+  },
+
+  footer: {
+    marginTop: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  iconButton: {
+    backgroundColor: 'gainsboro',
+    padding: 20,
+    borderRadius: 50,
   },
 });
